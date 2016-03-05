1e780a9fa4e3d9944ac75b24648ba01e44d5cf76
Matthew Daly <matthewd673@gmail.com>
2016-03-05 17:25:21 -0500

    Classes laid out, new window possibilities
    Physics class laid out, UI class laid out, fullscreen mode for windows
    is now available.

---
+++ neon2d/packages/n2d/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using neon2d;
+
+namespace n2d
+{
+    class Program
+    {
+
+        public static Window mywindow;
+        public static Scene myscene;
+
+        public static bool movingup = false;
+        public static int imagey = 10;
+
+        public static void Main(string[] args)
+        {
+            mywindow = new Window(800, 600, "my awesome window");
+            myscene = new Scene(mywindow);
+
+            Game maingame = new Game(mywindow, myscene, new Action(updateVoid));
+            maingame.runGame();
+        }
+
+        public static void updateVoid()
+        {
+            checkInput();
+
+            if(movingup)
+            {
+                imagey--;
+            }
+
+            Console.WriteLine("tick!");
+            Prop demoimage = new Prop(new Bitmap(@"C:\Users\Matthew\Documents\Visual Studio 2015\Projects\neon2d\neon2d\demoimage.png"), 10, imagey, 150, 150);
+            myscene.renderProp(demoimage);
+        }
+
+        public static void checkInput()
+        {
+            if(myscene.readKeyDown(Keys.Up))
+            {
+                movingup = true;
+            }
+
+            if(myscene.keyUp())
+            {
+                movingup = false;
+            }
+        }
+
+    }
+}
---
+++ neon2d/packages/neon2d/UI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neon2d
+{
+    public class UI
+    {
+
+        public class Button
+        {
+
+        }
+
+        public class TextBox
+        {
+
+        }
+
+        public class Label
+        {
+
+        }
+
+        public class ImageBox
+        {
+
+        }
+
+    }
+}

35b1b7e907a09f9ebfd1d156e4d5fb693e8bc912
Matthew Daly <matthewd673@gmail.com>
2016-03-07 15:11:45 -0500

    Math + Vector2 added

---
+++ neon2d/neon2d/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neon2d.Math
+{
+
+    public struct Vector2
+    {
+        int _x;
+        int _y;
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value;  }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+    }
+    
+}

75554f0a3ea9b0de47d14966cb4e080fd380069d
Matthew Daly <matthewd673@gmail.com>
2016-03-07 15:16:48 -0500

    Rectangle class added
    For rectangles, ellipses, and even triangles when rending Shapes

--- neon2d/neon2d/Math.cs
+++ neon2d/neon2d/Math.cs
@@ -25,5 +25,37 @@ namespace neon2d.Math
         }
 
     }
+
+    public struct Rectangle
+    {
+        int _x;
+        int _y;
+        int _w;
+        int _h;
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Width
+        {
+            get { return _w; }
+            set { _w = value; }
+        }
+
+        public int Height
+        {
+            get { return _h; }
+            set { _h = value; }
+        }
+    }
     
 }

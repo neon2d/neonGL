3e13b81de12fe47530c4a5efa22506de29fc92a1
Matthew Daly <matthewd673@gmail.com>
2016-03-16 14:38:01 -0400

    set up ParticleSystem
    everything but actual movement/rendering should be there

---
+++ neon2d/neon2d/ParticleSystem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using neon2d;
+using neon2d.Math;
+
+namespace neon2d
+{
+    class ParticleSystem
+    {
+
+
+        public int x;
+        public int y;
+
+        public float left;
+        public float right;
+        public float up;
+        public float down;
+        public int speed;
+
+        public object[] particles = new object[999999999];
+        public int particleCt = 0;
+
+        public ParticleSystem(int x, int y, float leftStrength, float rightStrength, float upStrenght, float downStrength, int movementspeed = 3)
+        {
+
+            this.x = x;
+            this.y = y;
+
+            left = leftStrength;
+            right = rightStrength;
+            up = upStrenght;
+            down = downStrength;
+            speed = movementspeed;
+        }
+        public ParticleSystem(int x, int y, Vector2f horizontalStrength, Vector2f verticalStrength, int movementspeed = 3)
+        {
+
+            this.x = x;
+            this.y = y;
+
+            left = horizontalStrength.x;
+            right = horizontalStrength.y;
+            up = verticalStrength.x;
+            down = verticalStrength.y;
+            speed = movementspeed;
+        }
+
+        public struct ParticleStruct
+        {
+            Object particleSource;
+            int x;
+            int y;
+            public ParticleStruct(Object particleSource, int x, int y)
+            {
+                this.particleSource = particleSource;
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public void addParticle(Prop particleSource)
+        {
+            particles[particleCt] = new ParticleStruct(particleSource, 0, 0);
+            particleCt++;
+        }
+        public void addParticle(Sprite particleSource)
+        {
+            particles[particleCt] = new ParticleStruct(particleSource, 0, 0);
+            particleCt++;
+        }
+
+    }
+}
